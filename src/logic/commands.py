a083c48ecf1581c1e94cd8bb9b71fa313f09dbba
YarielZC <yarielzamoracueto@gmail.com>
2025-11-29 14:10:19 -0500

    Modificar clean_command para que siempre retorne un dato tipo list[str]

--- src/logic/commands.py
+++ src/logic/commands.py
@@ -1,7 +1,7 @@
-def clean_command(command: str | list[str]):
+def clean_command(command: str | list[str]) -> list[str]:
 
     if type(command) is str:
-        return command[1:] if command[0]=='/' else command
+        return [command[1:] if command[0]=='/' else command]
     
     arr = [comm[1:] if comm[0]=='/' else comm for comm in command]
     
