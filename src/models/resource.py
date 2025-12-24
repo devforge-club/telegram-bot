a4691f7413fcdc65a72a44fd9c276e8106d0ddd0
YarielZC <yarielzamoracueto@gmail.com>
2025-12-24 14:09:39 -0500

    Modificar campo id a opcional tipo str en modelo resource

--- src/models/resource.py
+++ src/models/resource.py
@@ -1,10 +1,9 @@
 from datetime import datetime
-from typing import Self
 from uuid import uuid4
 from pydantic import BaseModel, Field
 
 class Resource(BaseModel):
-    id: str = Field(default_factory=lambda: str(uuid4()))
+    id: str | None = None
     title: str = Field(min_length=1, max_length=24)
     url: str
     category: str 
