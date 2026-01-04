faad865744cb9b3d47926556726d524c8bf7b598
Zenithar <borns1256@gmail.com>
2026-01-04 03:39:12 -0500

    Creado el metodo create

--- src/repositories/score_log_repository.py
+++ src/repositories/score_log_repository.py
@@ -9,3 +9,12 @@ class ScoreLogRepository(BaseRepository):
 
     def __init__(self, database: AsyncDatabase):
         super().__init__(database, "score_logs")
+        
+async def create(self, log: ScoreLog) -> ScoreLog:
+        log_dict = log.model_dump(exclude={"id"})
+        
+        result = await self.db.insert_one(log_dict)
+
+        log.id = str(result.inserted_id)
+
+        return log
