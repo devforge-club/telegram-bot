9b50fa4a6ac5eb3817959628bf03f73d97361ce8
SamuelCarriles <samuelcarrilesbanos@gmail.com>
2026-01-05 09:12:32 -0500

    feat(reminder_repository.py): Culminar implementación del script
    - Añadir get_due_reminders(self) para obtener un máximo de 100 recordatorios con fecha menor igual que la inmediata
    - Añadir delete(self, reminder_id: str) para eliminar un reminder de la db usando su id
    - Añadir get_by_user(self, telegram_id: str) para obtener un máximo de 100 recordatorios asociados a un usario específico

--- src/repositories/reminder_repository.py
+++ src/repositories/reminder_repository.py
@@ -1,6 +1,7 @@
 from pymongo.asynchronous.database import AsyncDatabase
 from ..models.reminder import Reminder
 from datetime import datetime
+from pymongo import ASCENDING
 from base_repository import BaseRepository
 
 
@@ -12,4 +13,20 @@ class ReminderRepository(BaseRepository):
         result = await self.db.insert_one(reminder.model_dump(exclude={"id"}))
         reminder.id = str(result.inserted_id)
         return reminder
-    
+
+    async def get_due_reminders(self) -> list[Reminder]:
+        docs = await self.db.find({"remind_at": {"$lte": datetime.now()}}).to_list(
+            length=100
+        )
+        return [Reminder(**self._map_doc(doc)) for doc in docs]
+
+    async def delete(self, reminder_id: str) -> bool:
+        oid = self._to_object_id(reminder_id)
+        if oid:
+            result = await self.db.delete_one({"_id": oid})
+            return result.deleted_count == 1
+        return False
+
+    async def get_by_user(self, telegram_id: str) -> list[Reminder]:
+        docs = await self.db.find({"user_id": telegram_id}).to_list(length=100)
+        return [Reminder(**self._map_doc(doc)) for doc in docs]
